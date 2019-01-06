fd9bef5a54e9b780be02dece877ff2ab5e1b730d
Valeriy Saphonov <xxrrrxx@yandex.ru>
2019-01-06 17:43:37 +0300

    add server + DB

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://shen:123456q@cluster0-q9gpp.mongodb.net/test?retryWrites=true",
+  { useNewUrlParser: true }
+);
+mongoose.connection
+  .once("open", () => {
+    console.log("connected to database");
+  })
+  .on("error", error => {
+    console.log("Connection error:", error);
+  });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening for requests on port 4000");
+});
